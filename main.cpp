4b5f25bee096fbbc5de1e983bd9cc6e7cff02be9
Kais Wass <kwassilima@gmail.com>
2023-04-08 14:09:11 +0200

    deplacement de perso

--- main.cpp
+++ main.cpp
@@ -16,30 +16,56 @@
     (a mettre plus tard)
 }*/
 
+/*void deplacement(){
 
+
+}*/
 
 // fonction main de base
 int main(){
 
     InitWindow(800,600, "mon jeux");
 
-    AfficherConsole();
+    //AfficherConsole();
 
     // load des fichiers / audios
-    Font font = LoadFont("font/Super-Mario-World.ttf");
 
-    
+    Vector2 position = { 356, 250 };
+
+    Texture2D player = LoadTexture("res/player.png");
+    Texture2D  mushroom = LoadTexture("res/mushroom.png");
+    Font font = LoadFont("font/Gotham-Medium.otf");
+    int PosX = GetRandomValue(0, GetScreenWidth() - mushroom.width);
+    int PosY = GetRandomValue(0, GetScreenHeight() - mushroom.height);
+    const float playerSpeed = 500.0f;
+    int vie = 0;
     // game loop
     while(!WindowShouldClose()){
         BeginDrawing();
         SetTraceLogLevel(LOG_NONE);
             ClearBackground(WHITE);
-            DrawTextEx(font ,"Salut tout le monde", (Vector2) {225, 246}, font.baseSize, 2, BLACK );
+            
+            float deltaTime = GetFrameTime();
+
+            DrawTextEx(font, TextFormat("Vie = %d", vie), (Vector2) {269, 28}, 42, 2, BLACK);
+
+
+            // deplacement
+            if (IsKeyDown(KEY_RIGHT)) position.x += playerSpeed * deltaTime;
+            if (IsKeyDown(KEY_LEFT)) position.x -= playerSpeed * deltaTime;
+            if (IsKeyDown(KEY_DOWN)) position.y += playerSpeed * deltaTime;
+            if (IsKeyDown(KEY_UP)) position.y -= playerSpeed * deltaTime;
+
+            
+            DrawTexture(mushroom, PosX, PosY, WHITE);
+            DrawTexture(player, position.x, position.y, WHITE);
+            //DrawTextEx(font ,"Salut tout le monde", (Vector2) {225, 246}, font.baseSize, 2, BLACK );
+
         EndDrawing();
     }
 
 
     // unload et fermetture de la fenetre
-    UnloadFont(font);
+    UnloadTexture(player);
     CloseWindow();
 }
