53810e80211e897e22ca10fcd94fcc9f3b2adf8e
Kais Wass <kwassilima@gmail.com>
2023-06-24 15:16:50 +0200

    Add Sdl3 support + WindowCreation

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,45 @@
-/******************************************
-*          Eat The Mushroom
-*               MIT
-*******************************************
-*   (c) Kaissou || All right reserved!
-*              main.cpp
-*******************************************
-*             03/06/2023
-*******************************************/
+//
+//  KaissouInc / KadjFoundation
+//          (c) 2023
+//  ---------------------------
+//      * main.cpp
+//
 
+// Example program:
+// Using SDL2 to create an application window
 
-#include "init.hpp"
-#include "input.hpp"
-#include <cmath>
+#include "SDL.h"
+#include <stdio.h>
 
-//load textures
+int main(int argc, char* argv[]) {
 
-using namespace std;
-using namespace fmt;
+    SDL_Window *window;                    // Declare a pointer
 
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
-void _DEBUG_(InitClass initclass){
-    print("InitWindow ..\n");
-    print("The Maximum framewrate is : 60.0fps\n");
-    print("Desktop Version\n");
-    
-    if (!initclass.mushroom.id || !initclass.player.id){
-        print("texture not load...\n");
-    }
-    else{
-        print("texture load !\n");
-    }
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
 
-}
-
-
-int main(int argc, char ** argv){
-
-    SetTraceLogLevel(LOG_NONE);
-    InitWindowClass initwindow;
-    initwindow.InitWindowFunctions();
-    InitClass initclass;
-    Move move;
-
-    _DEBUG_(initclass);
-
-    float distance = 0.0f;
-
-    initclass.GameStarted = false;
-
-    Vector2 position = {0, 500};
-    const float playerSpeed = 300.0f;
-
-    while(!WindowShouldClose()){
-        BeginDrawing();
-        if(initclass.GameStarted == false){
-            ClearBackground(BLACK);
-            initclass.TitleScreen();
-        }
-        else if(initclass.GameStarted == true)
-        {
-            float deltaTime = GetFrameTime();
-            ClearBackground(BLUE);
-            DrawText(TextFormat("Health = %d", initclass.vie), 269, 28, 42, BLACK);
-            DrawTexture(initclass.mushroom, initclass.PosX, initclass.PosY, WHITE);
-            DrawTexture(initclass.player, position.x, position.y, WHITE);
-        
-            if (IsKeyDown(KEY_RIGHT)) position.x += playerSpeed * deltaTime;
-            if (IsKeyDown(KEY_LEFT)) position.x -= playerSpeed * deltaTime;
-            if (IsKeyDown(KEY_DOWN)) position.y += playerSpeed * deltaTime;
-            if (IsKeyDown(KEY_UP)) position.y -= playerSpeed * deltaTime;
-            
-            //only work on windows!
-            distance = sqrt(pow(position.x - initclass.PosX, 2) + pow(position.y - initclass.PosY, 2));
-            if(distance < 50) {
-
-                initclass.vie++;
-
-                UnloadTexture(initclass.mushroom);
-
-       
-                initclass.PosX = GetRandomValue(0, GetScreenWidth() - initclass.mushroom.width);
-                initclass.PosY = GetRandomValue(0, GetScreenHeight() - initclass.mushroom.height);
-
-     
-                initclass.mushroom = LoadTexture("res/mushroom.png");
-                
-            }
-        
-        }
-        EndDrawing();
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
     }
 
-    UnloadTexture(initclass.mushroom);
-    CloseWindow();
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
     return 0;
 }
