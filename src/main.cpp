e1892dbdf8a5a92f37901ab9c0f7bbe0163d882c
Kais Wass <kwassilima@gmail.com>
2023-06-27 17:34:33 +0200

    added gameloop

--- src/main.cpp
+++ src/main.cpp
@@ -11,35 +11,50 @@
 #include "SDL.h"
 #include <stdio.h>
 #include <fmt/core.h>
+#include <boxer.h>
 
 int main(int argc, char* argv[]) {
 
     SDL_Window *window;                    // Declare a pointer
 
     SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-
+    SDL_Renderer* renderer;
+    SDL_Event event;
+    int isRunning = 1;
 
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_VULKAN                 // flags - see below
+        "Eat-The-Mushroom (5.X dev)",                  // window title
+        800,                               // width, in pixels
+        600,                               // height, in pixels
+        SDL_WINDOW_OPENGL                 // flags - see below
     );
 
-    fmt::print("hello window :)");
+
+    renderer = SDL_CreateRenderer(window, NULL, 0);
+
 
     // Check that the window was successfully created
     if (window == NULL) {
         // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
+        boxer::show("Could not create window", "Window error");
         return 1;
     }
 
-    // The window is open: could enter program loop here (see SDL_PollEvent())
+    while (isRunning) {
+        // Gestion des événements
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = 0;
+            }
+        }
 
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
-
+        // Effacer l'écran
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 161, 255);
+        // Mettre à jour l'affichage
+        SDL_RenderPresent(renderer);
+    }
     // Close and destroy the window
     SDL_DestroyWindow(window);
 
