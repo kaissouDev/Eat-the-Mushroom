53c4f1d4a0c43a7cbe113065fa42dd04e2d830ee
Kais Wass <kwassilima@gmail.com>
2023-07-08 01:20:05 +0200

    the refractorisation is now complete (except for the title screen, which will be back soon)

--- src/game.cpp
+++ src/game.cpp
@@ -3,6 +3,10 @@
 #include "player_rotation.hpp"
 #include <iostream>
 
+void Game::DrawObject(){
+    DrawText(TextFormat("Health = %d", Health), 269, 28, 42, WHITE);
+    DrawTexture(mushroom, PosX, PosY, WHITE);
+}
 
 void Game::TitleScreen(){
     ClearBackground(BLUE);
@@ -23,9 +27,28 @@ void Game::Gameloop(){
         float deltaTime = GetFrameTime();
         //TitleScreen();
         ClearBackground(BLUE);
+        DrawObject();
         drawPlayer(player, position, playerRotation);
         uint8_t playerDirection = pr.manageMovement( position, playerSpeed, deltaTime);
         pr.manageRotation(  playerRotation, playerDirection );
+
+        float distance = 0.0f;
+
+        distance = sqrt(pow(position.x - PosX, 2) + pow(position.y - PosY, 2));
+        if(distance < 50) {
+
+            Health++;
+
+            UnloadTexture(mushroom);
+
+       
+            PosX = GetRandomValue(0, GetScreenWidth() - mushroom.width);
+            PosY = GetRandomValue(0, GetScreenHeight() - mushroom.height);
+
+     
+            mushroom = LoadTexture("res/mushroom.png");
+                
+        }
 
         EndDrawing();
     }
@@ -33,8 +56,22 @@ void Game::Gameloop(){
 
 void Game::CreateWindow(){
     InitWindow(800, 600, "Eat-the-Mushroom");
-    mushroom = LoadTexture("res/mushroom.png");
-    player = LoadTexture("res/player.png");
+    
+    // set the window icon
+    icon = LoadImage("res/icon.png");
+    SetWindowIcon(icon);
+
+    // check operating system
+    #if defined(_WIN32) || defined(__linux__)
+        mushroom = LoadTexture("res/mushroom.png");
+        player = LoadTexture("res/player.png");
+        fmt::print("Load from Windows or Linux");
+    #elif defined(__APPLE__)
+        mushroom = LoadTexture("../Resources/res/res/mushroom.png");
+        player = LoadTexture("../Resources/res/res/player.png");
+        fmt::print("Load from macOS");
+    #endif
+    
     Gameloop();
     UnloadTexture(player);
     UnloadTexture(mushroom);
--- src/game.hpp
+++ src/game.hpp
@@ -4,13 +4,17 @@ class Game{
     public:
         Texture2D mushroom;
         Texture2D player;
+        Image icon;
         int PosX;
         int PosY;
+        int Health;
+
         uint16_t playerRotation = 0;
         bool GameStarted;
         void CreateWindow();
         void Gameloop();
         void TitleScreen();
+        void DrawObject();
         static void drawPlayer ( Texture2D &Texture, Vector2 &position, uint16_t rotation )
         {
 	        Vector2 newPosition = position;
